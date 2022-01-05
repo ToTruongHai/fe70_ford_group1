a26a7e27d48ddece59118c8f31b582ad3cbc8b3b
Tran Quang Huy <tranquanghuy12h@gmail.com>
2022-01-05 16:43:07 +0700

    tqhuy updated header and blue scale button

--- js/mainScript.js
+++ js/mainScript.js
@@ -1,12 +1,61 @@
-// VIEW ALL 
-let viewAll = document.querySelectorAll('.toggle');
+// VIEW ALL
+let viewAll = document.querySelectorAll(".toggle");
 for (let i = 0; i < viewAll.length; i++) {
-  viewAll[i].addEventListener('click',function(){
-    viewAll[i].parentNode.classList.toggle('active');
-  })
+  viewAll[i].addEventListener("click", function () {
+    viewAll[i].parentNode.classList.toggle("active");
+  });
 }
 //END VIEW-ALL
 
+// START HIDE HEADER ON SCOLL DOWN AND SHOW ON SCOLL UP
+(function () {
+  var doc = document.documentElement;
+  var w = window;
+
+  var curScroll;
+  var prevScroll = w.scrollY || doc.scrollTop;
+  var curDirection = 0;
+  var prevDirection = 0;
+
+  var header = document.getElementById("site-header");
+  var toggled;
+  var threshold = 200;
+
+  var checkScroll = function () {
+    curScroll = w.scrollY || doc.scrollTop;
+    if (curScroll > prevScroll) {
+      // scrolled down
+      curDirection = 2;
+    } else {
+      //scrolled up
+      curDirection = 1;
+    }
+
+    if (curDirection !== prevDirection) {
+      toggled = toggleHeader();
+    }
+
+    prevScroll = curScroll;
+    if (toggled) {
+      prevDirection = curDirection;
+    }
+  };
+
+  var toggleHeader = function () {
+    toggled = true;
+    if (curDirection === 2 && curScroll > threshold) {
+      header.classList.add("hide");
+    } else if (curDirection === 1) {
+      header.classList.remove("hide");
+    } else {
+      toggled = false;
+    }
+    return toggled;
+  };
+
+  window.addEventListener("scroll", checkScroll);
+})();
+// END HIDE AND SHOW HEADER ON SCOLL
 
 // START FOOTER CATEGORY SCRIPT
 window.onresize = function () {
@@ -24,16 +73,16 @@ let toolTitle = document.getElementsByClassName("category__title-tool");
 
 // khoa-sudo
 let newsCollapse = document.getElementById("collapseNews");
-let newsViewAllCollapseBtn = document.getElementById("newsViewAllCollapseBtn")
+let newsViewAllCollapseBtn = document.getElementById("newsViewAllCollapseBtn");
 // khoa-sudo
 
 function categoryResponsive() {
-  let screenWidth = $(document).width()+17;
+  let screenWidth = $(document).width() + 17;
   if (screenWidth < 1200) {
     $(aClickable).attr("data-toggle", "collapse");
     $(divCollapse).addClass("collapse");
     $(itemMD).removeClass("row");
-    } else {
+  } else {
     $(aClickable).removeAttr("data-toggle");
     $(divCollapse).removeClass("collapse");
     $(itemMD).addClass("row");
